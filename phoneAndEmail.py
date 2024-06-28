8e6da5889978173967574b8572949ba77d51e9fc
weisje <20269574+weisje@users.noreply.github.com>
2024-06-28 07:32:52 -0400

    Added TODO blocks for tasks to be accomplished in the code.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -1,6 +1,17 @@
 
-
 def main():
+    # TODO Get the text off of the clipboard
+        # TODO Use pyperclip to copy and paste information from clipboard
+
+    # TODO Find all the phone numbers and emails in the text
+        # TODO Create a regex for matching phone numbers
+        # TODO Create a regex for matching email addresses
+        # TODO Find all (not just first) matches of above regexes
+        # TODO Neatly format found information into a single string
+
+    # TODO Paste the found information onto the clipboard
+        # TODO Provide message for if information was found or not
+        # TODO Paste information to clipboard
     pass
 
 
