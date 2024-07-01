7cd70e02c418b907e757b9b705d92e9d291ae89a
weisje <20269574+weisje@users.noreply.github.com>
2024-07-01 07:34:23 -0400

    Constructed regex pattern for finding email addresses.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -7,17 +7,24 @@ def main():
         # TODO Use pyperclip to copy and paste information from clipboard
 
     # TODO Find all the phone numbers and emails in the text
-        # Regex pattern for finding phone numbers:
-        phoneNumber = re.compile(r'''(
-            (\d{3}|\(\d{3}\))?              # Area code
-            (\s|-|\.)?                      # Seperator
-            \d{3}                           # First three digits
-            (\s|-|\.)                       # Seperator
-            \d{4}                           # Last four digits
-            (\s*(ext|x|ext.)\s*\d{2,5})?    # Extension
-        )''', re.VERBOSE)
+    # Regex pattern for finding phone numbers:
+    phoneRegex = re.compile(r'''(
+        (\d{3}|\(\d{3}\))?              # Area code
+        (\s|-|\.)?                      # Seperator
+        \d{3}                           # First three digits
+        (\s|-|\.)                       # Seperator
+        \d{4}                           # Last four digits
+        (\s*(ext|x|ext.)\s*\d{2,5})?    # Extension
+    )''', re.VERBOSE)
 
-        # TODO Create a regex for matching email addresses
+    # Regex pattern for matching email addresses:
+    emailRegex = re.compile(r'''(
+        [a-zA-Z0-9._%+-]+   # Username
+        (@|at)              # @ symbol 
+        [a-zA-Z0-9.-]+      # Domain name
+        (\.[a-zA-Z]{2,4})   # dot-something
+    )''', re.VERBOSE) # (|\[at]|\s\[\sat\s]|\s\[at]\s|\(at\)|\s\(at\)\s|\s\(\sat\s\))
+
         # TODO Find all (not just first) matches of above regexes
         # TODO Neatly format found information into a single string
 
