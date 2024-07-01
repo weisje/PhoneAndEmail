d4f490708c689efc2db07f734ce76bf3e1f57ecd
weisje <20269574+weisje@users.noreply.github.com>
2024-07-01 07:23:03 -0400

    Imported pyperclip module.  Constructed regex pattern for finding phone numbers.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -1,10 +1,22 @@
+import pyperclip
+import re
+
 
 def main():
     # TODO Get the text off of the clipboard
         # TODO Use pyperclip to copy and paste information from clipboard
 
     # TODO Find all the phone numbers and emails in the text
-        # TODO Create a regex for matching phone numbers
+        # Regex pattern for finding phone numbers:
+        phoneNumber = re.compile(r'''(
+            (\d{3}|\(\d{3}\))?              # Area code
+            (\s|-|\.)?                      # Seperator
+            \d{3}                           # First three digits
+            (\s|-|\.)                       # Seperator
+            \d{4}                           # Last four digits
+            (\s*(ext|x|ext.)\s*\d{2,5})?    # Extension
+        )''', re.VERBOSE)
+
         # TODO Create a regex for matching email addresses
         # TODO Find all (not just first) matches of above regexes
         # TODO Neatly format found information into a single string
@@ -12,7 +24,6 @@ def main():
     # TODO Paste the found information onto the clipboard
         # TODO Provide message for if information was found or not
         # TODO Paste information to clipboard
-    pass
 
 
 if __name__ == '__main__':
