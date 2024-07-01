fcaca1ebfcefbe202d70a0074e4fa7c127ab9003
weisje <20269574+weisje@users.noreply.github.com>
2024-07-01 07:54:11 -0400

    Was able to copy wall of text info into program & have it seek out phone numbers as well as emails.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -9,19 +9,19 @@ def main():
     phoneRegex = re.compile(r'''(
         (\d{3}|\(\d{3}\))?              # Area code
         (\s|-|\.)?                      # Seperator
-        \d{3}                           # First three digits
+        (\d{3})                           # First three digits
         (\s|-|\.)                       # Seperator
-        \d{4}                           # Last four digits
+        (\d{4})                           # Last four digits
         (\s*(ext|x|ext.)\s*\d{2,5})?    # Extension
     )''', re.VERBOSE)
 
     # Regex pattern for matching email addresses:
     emailRegex = re.compile(r'''(
         [a-zA-Z0-9._%+-]+   # Username
-        (@|at)              # @ symbol 
+        (@|\[at])                   # @ symbol 
         [a-zA-Z0-9.-]+      # Domain name
         (\.[a-zA-Z]{2,4})   # dot-something
-    )''', re.VERBOSE) # (|\[at]|\s\[\sat\s]|\s\[at]\s|\(at\)|\s\(at\)\s|\s\(\sat\s\))
+    )''', re.VERBOSE) # (|\sat\s|\s\[\sat\s]|\s\[at]\s|\(at\)|\s\(at\)\s|\s\(\sat\s\))
 
     text = str(pyperclip.paste())
 
@@ -29,17 +29,18 @@ def main():
     for groups in phoneRegex.findall(text):
         phoneNum = '-'.join([groups[1], groups[3], groups[5]])
         if groups[6] != "":
-            phoneNum += 'x' + groups[6]
+            phoneNum += ' x' + groups[6]
         matches.append(phoneNum)
 
     for groups in emailRegex.findall(text):
-        matches.append(groups)
+        matches.append(groups[0])
 
-        # TODO Neatly format found information into a single string
-
-    # TODO Paste the found information onto the clipboard
-        # TODO Provide message for if information was found or not
-        # TODO Paste information to clipboard
+    if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Copied to clipboard:')
+        print('\n'.join(matches))
+    else:
+        print("No phone numbers or email addresses found.")
 
 
 if __name__ == '__main__':
