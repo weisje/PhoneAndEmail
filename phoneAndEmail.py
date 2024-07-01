83b24315f5a40814cb469d254507440140b82181
weisje <20269574+weisje@users.noreply.github.com>
2024-07-01 07:40:17 -0400

    Used pyperclip to paste clipboard to program. Searched through clipboard text for email & phone numbers.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -3,8 +3,6 @@ import re
 
 
 def main():
-    # TODO Get the text off of the clipboard
-        # TODO Use pyperclip to copy and paste information from clipboard
 
     # TODO Find all the phone numbers and emails in the text
     # Regex pattern for finding phone numbers:
@@ -25,7 +23,18 @@ def main():
         (\.[a-zA-Z]{2,4})   # dot-something
     )''', re.VERBOSE) # (|\[at]|\s\[\sat\s]|\s\[at]\s|\(at\)|\s\(at\)\s|\s\(\sat\s\))
 
-        # TODO Find all (not just first) matches of above regexes
+    text = str(pyperclip.paste())
+
+    matches = []
+    for groups in phoneRegex.findall(text):
+        phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+        if groups[6] != "":
+            phoneNum += 'x' + groups[6]
+        matches.append(phoneNum)
+
+    for groups in emailRegex.findall(text):
+        matches.append(groups)
+
         # TODO Neatly format found information into a single string
 
     # TODO Paste the found information onto the clipboard
